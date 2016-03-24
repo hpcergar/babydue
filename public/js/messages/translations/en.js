2d9cf85fc2a004304e8b0a05e256aeaea9dab46c
hpcergar <jek_@hotmail.com>
2016-03-24 19:23:41 +0000

    cleaning webpack config

---
+++ public/js/messages/translations/en.js
@@ -0,0 +1,123 @@
+define([], function () {
+
+    return {
+        "OK": "OK",
+        "Are you sure?": "",
+        "Delete": "",
+        "Delete bookmark": "",
+        "Delete folder": "",
+        "Select folder": "",
+        "Tablet": "",
+        "Phone": "",
+        "Classic": "",
+        "Password": "",
+        "token_fail": "Impossible to authentificate",
+        "Not signed in": "",
+        "Select doc from the list": "Select a document first",
+        "Define search first": "Please start a search first",
+        "No results": "",
+        "Select all": "",
+        "Demo": "",
+
+        "Home": "",
+        "Search": "",
+        "Advanced search": "",
+        "History": "",
+        "Documents": "",
+        "Queries": "",
+        "Folders": "",
+        "My history": "",
+        "My folders": "",
+        "Search history": "",
+        "Query": "",
+        "Search": "",
+        "New search": "",
+        "Search results": "",
+        "Results": "",
+        "Keyword": "",
+        "Sign In": "",
+        "Sign Out": "",
+
+        // "matieres": "Matières",
+        // "doc_type": "Type",
+        // "doc_year": "Année",
+        // "doc_lng": "Langue",
+
+        "no_message": "Unknown error. Please contact us at email <a href=\"mailto:info@APP_NAME_DOMAIN\">info@APP_NAME_DOMAIN</a>. We apologize for any inconvenience",
+        "invalid_request": "Unknown error (invalid_request)",
+        "invalid_client": "Unknown error (invalid_client)",
+        // "invalid_grant": "Unknown error (invalid_grant)",
+        "invalid_grant": "Login or password error",
+        "unauthorized_client": "Unknown error (unauthorized_client)",
+        "unsupported_grant_type": "Unknown error (unsupported_grant_type)",
+        "invalid_scope": "Unknown error (invalid_scope)",
+
+        "[Bookmark search]": "Bookmark request",
+        "[meta]": "Document information",
+        "Result": "",
+        "Linked Documents": "",
+
+        "Interest calculator": "",
+
+        "Error": "",
+        "News": " ",
+        "Monitor": "Belgian Official Journal",
+        "NewsDoc": " News",
+        "timeout": "",
+        "Title": "",
+        "Reference": "",
+        "Note": "",
+        "More results": "",
+        "Authentication needed": "",
+        "Unsorted bookmarks": "",
+        "Folder not selected": "",
+        "By relevance": "",
+        "By date": "",
+        "New folder": "",
+        "Folder": "",
+        "Comments": "",
+        "Bookmark": "",
+        "Cancel": "",
+        "Repeat": "",
+        "doc content": "Document content",
+        "lang": "FR/NL",
+        "langSL": "EN/FR/NL",
+        "copyright": "Larcier Group - 2015",
+        "You have been disconnected": "",
+        "not_supported": "Your platform is not supported. Functionality of mobile APP_NAME might be limited. For best possible experience we recommend recent versions of Apple iOS or Google Android with recent version of Safari, Chrome or Firefox.",
+
+        "PDF document is open in a new browser window.": "",
+        "Please allow popups to view PDF documents.": "",
+        "Login": "Email",
+
+
+        //
+        // tour
+        //
+        "tour_0": "Glissez à gauche pour passer à l’écran suivant<br>Glissez à droite pour passer à l’écran précédent<br>Touchez la croix dans le coin supérieur gauche de l’écran pour fermer cette visite guidée",
+        "tour_1_1": "Un design adapté à tous vos écrans",
+        "tour_1_2": "Nouvel affichage en deux colonnes optimisé pour tablettes",
+        "tour_1_3": "Choisissez la configuration qui correspond à votre appareil via le menu : un affichage en une colonne pour votre smartphone ou en deux colonnes pour votre tablette.",
+
+        "tour_2_1": "Une version mobile 100% tactile",
+        "tour_2_2": "Une nouvelle navigation plus rapide, qui suit le mouvement de vos doigts",
+        "tour_2_3": "Faites glisser l’écran à gauche ou à droite pour naviguer. Glissez ainsi rapidement de votre recherche aux résultats et des résultats aux documents.",
+
+        "tour_3_1": "Une gestion plus facile des dossiers de favoris",
+        "tour_3_2": "Un contrôle total de vos dossiers APP_NAME_MYDBPRO",
+        "tour_3_3": "Ajoutez, modifiez ou supprimez vos favoris et dossiers de favoris directement depuis la version mobile.",
+
+
+        //
+        // sources
+        //
+        "mono": "Bookshelf",
+        "comm": "Commentaires",
+        // "*_mono_*" -> t("mono") + info.products["*_mono_*"], same for comm
+
+        "fn_rech": "Advanced search",
+        "ca_rech": "Advanced search",
+        "sw_rech": "Advanced search",
+    }
+
+});
---
+++ public/js/messages/translations/nl.js
@@ -0,0 +1,121 @@
+define([], function () {
+
+    return {
+        "OK": "OK",
+        "Are you sure?": "Bevestigen",
+        "Delete": "Verwijderen",
+        "Delete bookmark": "Van favorieten verwijderen",
+        "Delete folder": "Map verwijderen",
+        "Select folder": "Map selecteren",
+        "Tablet": "Tablet",
+        "Phone": "Smartphone",
+        "Classic": "Klassiek",
+        "Password": "Wacthwoord",
+        "token_fail": " Aanmelden onmogelijk ",
+        "Not signed in": "Niet aangemeld",
+        "Select doc from the list": "Gelieve een document uit de lijst te selecteren",
+        "Define search first": "Gelieve eerst een zoekopdracht uit te voeren",
+        "No results": "Uw zoekopdracht heeft 0 resultaten opgeleverd.",
+        "Select all": "Alles selecteren",
+        "Demo": "Demo",
+
+        "Home": "Startpagina",
+        "Search": "Zoeken",
+        "Advanced search": "Geavanceerd zoeken",
+        "Search history": "Historiek van zoekopdrachten",
+        "History": "Historiek",
+        "Documents": "Documenten",
+        "Queries": "Zoekopdrachten",
+        "Folders": "Mappen",
+        "My history": "Mijn historiek",
+        "My folders": "Mijn mappen",
+        "Query": "Zoekopdracht",
+        "New search": "Nieuwe zoekopdracht",
+        "Search results": "Zoekresultaten",
+        "Results": "Resultaten",
+        "Keyword": "Trefwoord",
+        "Sign In": "Abonnee toegang",
+        "Sign Out": "Uitloggen",
+        "matieres": "Thema's",
+        "doc_type": "Type",
+        "doc_year": "Jaar",
+        "doc_lng": "Taal",
+        "no_message": "Er is een fout opgetreden. Gelieve ons hiervoor te verontschuldigen. Als deze fout zich blijft voordoen, stuur een e-mail naar <a href=\"mailto:info@APP_NAME_DOMAIN\">info@APP_NAME_DOMAIN</a> met de beschrijving van hoe deze fout is opgetreden. Alvast bedankt voor uw begrip.",
+        "invalid_request": "Onverwachte fout (invalid_request)",
+        "invalid_client": "Onverwachte fout (invalid_client)",
+        // "invalid_grant": "Onverwachte fout (invalid_grant)",
+        "invalid_grant": "Fout bij aanmelden of foutief wachtwoord",
+        "unauthorized_client": "Onverwachte fout (unauthorized_client)",
+        "unsupported_grant_type": "Onverwachte fout (invalid_scope)",
+        "invalid_scope": "Onverwachte fout (invalid_scope)",
+
+        "[Bookmark search]": "Zoekopdracht bewaren",
+        "[meta]": "Informatie over het document",
+        "Result": "Resultaat",
+        // "Linked Documents": "Documents liés",
+
+        "Interest calculator": "Rente calculator",
+
+        "Error": "Fout",
+        "News": " Actualiteiten",
+        "Monitor": " Staatsblad",
+        "NewsDoc": " Actualiteit",
+        "timeout": "Timeout. Probeer het later opnieuw",
+        "Title": "Titel",
+        "Reference": " Verwijzing",
+        "Note": "Noot",
+        "More results": "Meer resultaten",
+        "Authentication needed": "Gelieve u aan te melden om tot deze functionaliteit toegang te krijgen",
+        "Unsorted bookmarks": "Niet-geklasseerde favorieten",
+        "Folder not selected": "In een map klasseren",
+        "By relevance": "Sorteren op relevantie",
+        "By date": "Op datum sorteren",
+        "New folder": "Nieuwe map",
+        "Folder": "Map",
+        "Comments": "Commentaren",
+        "Bookmark": "Favorieten",
+        "Cancel": "Annuleren",
+        "Repeat": "Herhalen",
+        "doc content": "Volledige tekst",
+        "lang": "NL/FR",
+        "langSL": "EN/FR/NL",
+        "copyright": "Groep Larcier - 2014",
+        "You have been disconnected": "U bent uitgelogd",
+        "not_supported": "Uw platform wordt niet ondersteund. Daarom kunnen wij de optimale werking van de mobiele APP_NAME niet garanderen. Voor een betere ervaring, raden we u aan om de recentste versies van Apple iOS en Google Android, evenals de recentste versies van Safari, Chrome of Firefox te gebruiken.",
+        "PDF document is open in a new browser window.": "Het PDF-document wordt in een nieuwe browservenster geopend.",
+        "Please allow popups to view PDF documents.": "Gelieve de pop-upblokkering van uw browser uit te schakelen om PDF-documenten te kunnen openen.",
+        "Login": "Email",
+
+
+        //
+        // tour
+        //
+        // "tour_0": "Schuif naar links om naar het volgende scherm te gaan.<br>Schuif naar rechts om naar het vorige scherm te gaan.<br>Raak het kruisje in de linkerbovenhoek van het scherm aan om deze demo af te sluiten.",
+        "tour_0": "Schuif naar links voor volgende scherm.<br>Schuif naar rechts voor vorige scherm.<br>Raak het kruisje in de linkerbovenhoek van het scherm aan om deze demo af te sluiten.",
+
+        "tour_1_1": "Een design geschikt voor al uw schermen",
+        "tour_1_2": "Geoptimaliseerde nieuwe weergave in twee kolommen voor tablets",
+        "tour_1_3": "Kies de configuratie die bij uw toestel past via het menu: een weergave in één kolom voor uw smartphone of in twee kolommen voor uw tablet.",
+
+        "tour_2_1": "Een 100% tactiele mobiele versie",
+        "tour_2_2": "Een nieuwe, snellere navigatie die de beweging van uw vinger volgt",
+        "tour_2_3": "Schuif het scherm naar links of naar rechts om te navigeren. Ga zo snel van uw zoekopdracht naar de resultaten en van de resultaten naar de documenten.",
+
+        "tour_3_1": "Eenvoudiger beheer van favoriete mappen",
+        "tour_3_2": "Totale controle over uw APP_NAME_MYDBPRO-mappen",
+        "tour_3_3": "Voeg toe, wijzig of verwijder uw favorieten en favoriete mappen rechtstreeks vanuit de mobiele versie.",
+
+
+        //
+        // sources
+        //
+        "mono": "Boekenplank",
+        "comm": "Commentaren",
+        // "*_mono_*" -> t("mono") + info.products["*_mono_*"], same for comm
+
+        "fn_rech": "Geavanceerd zoeken",
+        "ca_rech": "Geavanceerd zoeken",
+        "sw_rech": "Geavanceerd zoeken",
+    }
+
+});
